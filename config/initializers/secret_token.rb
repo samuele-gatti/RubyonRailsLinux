110bf4126772c882f6f242256c4492967e2186e9
Samuele <samuele.gatti@studiofarma.it>
2013-05-08 23:49:32 +0200

    commit azure

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+BlogApp::Application.config.secret_token = '535231779e3a660fd572382bebd1d6bbf4179cfd3b62cd52d5ee26913dabcd699ef967562a3efbd4b3305b9aca41771e90aeab1b2095aa52a8d88e8bff362563'
